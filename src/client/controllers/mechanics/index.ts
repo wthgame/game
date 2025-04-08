cbab3f776183fca4e64808adf2fe95fcaedf7eb7
znotfireman <znotfireman@gmail.com>
2025-04-08 14:38:25 +0700

    feat: getAllAttributes

--- src/client/controllers/mechanics/index.ts
+++ src/client/controllers/mechanics/index.ts
@@ -2,8 +2,9 @@ import { Controller, OnInit, OnStart } from "@flamework/core";
 import { Lazy } from "@rbxts/lazy";
 import ty from "@rbxts/libopen-ty";
 import { ReplicatedStorage, Workspace } from "@rbxts/services";
+import { Trove } from "@rbxts/trove";
 import { panic } from "shared/flamework";
-import { debug, info, trace, warn } from "shared/log";
+import { debug, err, info, trace, warn } from "shared/log";
 
 export interface Mechanic<A extends Record<string, AttributeValue>, I extends Instance> {
 	type: "Mechanic";
@@ -35,8 +36,15 @@ export interface MechanizeScope<A extends Record<string, AttributeValue>, I exte
 	id: number;
 	instance: I;
 	mechanicsFolder: Instance;
-	getAttribute<T extends keyof A>(attribute: T): A[T];
-	pushTasks<Tasks extends defined[]>(...tasks: Tasks): LuaTuple<Tasks>;
+	getAttribute: <T extends keyof A>(attribute: T) => A[T];
+	getAllAttributes: () => A;
+	trove: Trove;
+
+	logTrace: (...args: defined[]) => void;
+	logDebug: (...args: defined[]) => void;
+	logInfo: (...args: defined[]) => void;
+	logWarn: (...args: defined[]) => void;
+	logError: (...args: defined[]) => void;
 }
 
 type Tag = string;
@@ -101,7 +109,7 @@ export class MechanicsController implements OnInit, OnStart {
 		info("Loading mechanics from", mechanicsFolder.GetFullName());
 		let id = 0;
 
-		const cleanupTasks = new Array<defined>();
+		const trove = new Trove();
 
 		for (const descendant of mechanicsFolder.GetDescendants()) {
 			const mechanicsToLoad = new Array<AnyMechanic>();
@@ -119,27 +127,57 @@ export class MechanicsController implements OnInit, OnStart {
 			const mechId = id++;
 
 			for (const description of mechanicsToLoad) {
+				const mechanicTrove = trove.extend();
+
 				task.spawn(
 					(description, mechanicsFolder, instance, id) => {
 						trace("Mechanizing description", description.name);
-						description.mechanize({
-							mechanicsFolder,
-							instance,
-							id,
+						description.mechanize(
+							table.freeze({
+								mechanicsFolder,
+								instance,
+								id,
 
-							getAttribute(attribute: string): AttributeValue {
-								const instanceAttributeNow = instance.GetAttribute(attribute) as never;
-								const defaultAttribute = description.defaultAttributes[attribute];
-								return instanceAttributeNow || defaultAttribute;
-							},
+								getAttribute: (attribute: string): AttributeValue => {
+									const instanceAttributeNow = instance.GetAttribute(attribute) as never;
+									const defaultAttribute = description.defaultAttributes[attribute];
+									return instanceAttributeNow || defaultAttribute;
+								},
 
-							pushTasks<Tasks extends defined[]>(...tasks: Tasks): LuaTuple<Tasks> {
-								for (const task of tasks) {
-									cleanupTasks.push(task);
-								}
-								return $tuple(...tasks);
-							},
-						});
+								getAllAttributes: () => {
+									const instanceAttributes = instance.GetAttributes();
+									const reconciledAttributes = new Map();
+
+									for (const [k, v] of pairs(description.defaultAttributes)) {
+										reconciledAttributes.set(k, instanceAttributes.get(k) || v);
+									}
+
+									return reconciledAttributes as never;
+								},
+
+								trove,
+
+								logTrace: (...args) => {
+									trace(`Mechanic:${description.name} -`, ...args);
+								},
+
+								logDebug: (...args) => {
+									debug(`Mechanic:${description.name} -`, ...args);
+								},
+
+								logInfo: (...args) => {
+									info(`Mechanic:${description.name} -`, ...args);
+								},
+
+								logWarn: (...args) => {
+									warn(`Mechanic:${description.name} -`, ...args);
+								},
+
+								logError: (...args) => {
+									err(`Mechanic:${description.name} -`, ...args);
+								},
+							}),
+						);
 					},
 					description,
 					mechanicsFolder,
