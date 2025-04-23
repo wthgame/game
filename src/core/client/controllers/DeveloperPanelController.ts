1ab4ac0ed1962db3ddde7b7780a98907f56a1505
znotfireman <znotfireman@gmail.com>
2025-04-23 08:13:02 +0700

    feat: DeveloperPanelController

---
+++ src/core/client/controllers/DeveloperPanelController.ts
@@ -0,0 +1,42 @@
+import { Controller, Modding, OnStart } from "@flamework/core";
+import Iris from "@rbxts/iris";
+import { RunService, UserInputService } from "@rbxts/services";
+
+export interface DeveloperPanelDropdownRenderer {
+	renderDeveloperPanelDropdown(): void;
+}
+
+@Controller()
+export class DeveloperPanelController implements OnStart {
+	private open = RunService.IsStudio() || !RunService.IsRunning();
+	private dropdownRenderers = new Array<DeveloperPanelDropdownRenderer>();
+
+	onStart(): void {
+		Modding.onListenerAdded<DeveloperPanelDropdownRenderer>((object) => this.dropdownRenderers.push(object));
+		Modding.onListenerRemoved<DeveloperPanelDropdownRenderer>((object) =>
+			this.dropdownRenderers.remove(this.dropdownRenderers.indexOf(object)),
+		);
+
+		this.dropdownRenderers.sort((lhs, rhs) => tostring(lhs) < tostring(rhs));
+
+		Iris.Init();
+		Iris.UpdateGlobalConfig(Iris.TemplateConfig.colorDark);
+		Iris.UpdateGlobalConfig(Iris.TemplateConfig.sizeClear);
+		Iris.Connect(() => this.render());
+
+		UserInputService.InputBegan.Connect((input, gp) => {
+			if (!gp && input.KeyCode === Enum.KeyCode.Comma) this.open = !this.open;
+		});
+	}
+
+	render() {
+		if (!this.open) return;
+		Iris.Window(["Welcome To Hell Developer Panel"]);
+		for (const renderer of this.dropdownRenderers) {
+			Iris.Tree([tostring(renderer)]);
+			renderer.renderDeveloperPanelDropdown();
+			Iris.End();
+		}
+		Iris.End();
+	}
+}
--- src/kit/client/controllers/KitTowerClient.ts
+++ src/kit/client/controllers/KitTowerClient.ts
@@ -1,12 +1,16 @@
 import { Controller, OnStart } from "@flamework/core";
+import Iris from "@rbxts/iris";
 import { Workspace } from "@rbxts/services";
 import { Trove } from "@rbxts/trove";
+import { DeveloperPanelDropdownRenderer } from "core/client/controllers/DeveloperPanelController";
 import { MechanicController } from "core/client/controllers/MechanicController";
 import { onFlameworkExtinguished } from "core/shared/flamework";
 import { confirmKitLoaded, loadKitMechanics } from "../net";
 
 @Controller()
-export class KitTowerClient implements OnStart {
+export class KitTowerClient implements OnStart, DeveloperPanelDropdownRenderer {
+	private isLoaded = false;
+
 	constructor(private mechanicController: MechanicController) {}
 
 	async onStart(): Promise<void> {
@@ -20,7 +24,12 @@ export class KitTowerClient implements OnStart {
 			const trove = new Trove();
 			this.mechanicController.loadMechanicsFromParent(trove, clonedMechanics);
 
+			this.isLoaded = true;
 			onFlameworkExtinguished(() => trove.clean());
 		}
+	}
+
+	renderDeveloperPanelDropdown(): void {
+		Iris.Text([this.isLoaded ? "Mechanics are loaded" : "Mechanics are NOT loaded"]);
 	}
 }
